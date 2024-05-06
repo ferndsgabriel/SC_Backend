162ab5249cfa4270200cd74607bfca1333efba43
gabriel da silva <gabrielsilvafernandes1606@gmail.com>
2024-05-06 17:34:20 -0300

    try fix nodemailer at vercel

--- src/utils/SendEmail.ts
+++ src/utils/SendEmail.ts
@@ -7,25 +7,34 @@ export async function SendEmail(email:string, message:string) {
     const user = process.env.NODEMAILER_EMAIL;
     const pass = process.env.NODEMAILER_PASS;
     
-    try {	   
-        await nodemailer
-            .createTransport({
-                host: 'smtpserver.com',
-                port: 465,
-                secure: true,
-                auth: {
-                user: user,
-                pass:pass
-                },
-            })
-        .sendMail({
+    const transporter = nodemailer.createTransport({
+        host:"smtp.gmail.com",
+        secure:false,
+        port:587 ,
+        auth:{user, pass}
+    });
+    await new Promise((resolve, reject) => {
+        // verify connection configuration
+        transporter.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
+    });
+    await new Promise((resolve, reject) => {
+        transporter.sendMail({
             from:user,
             to:myEmail,
+            subject:'Email de salãoCondo',
             html:message
+        }).then((inf)=>{
+            console.log('sucess');
+        }).catch((e)=>{
+            console.log(e)
         })
-        console.log('Email sent to ' + email)
-    } catch (e) {
-        console.error(e)
-    }
+    });
 }     
-
