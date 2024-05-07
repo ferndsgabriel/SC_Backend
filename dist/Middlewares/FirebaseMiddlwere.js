150405cbe12d52381a9dceb78b4c42e84a1b53df
gabriel da silva <gabrielsilvafernandes1606@gmail.com>
2024-05-06 21:40:44 -0300

    try change pass

--- dist/Middlewares/FirebaseMiddlwere.js
+++ dist/Middlewares/FirebaseMiddlwere.js
@@ -13,7 +13,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const firebase_admin_1 = __importDefault(require("firebase-admin"));
-const serviceAccount = require("");
+const serviceAccount = require("../config/firebaseKeySC.json");
 firebase_admin_1.default.initializeApp({
     credential: firebase_admin_1.default.credential.cert(serviceAccount),
     storageBucket: 'gs://salaocondo.appspot.com',
