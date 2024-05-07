32cf7b4d9954fd061f01d07a4e36835dd717bc1e
gabriel da silva <gabrielsilvafernandes1606@gmail.com>
2024-05-06 21:44:01 -0300

    try change pass

--- dist/utils/FormatEmail.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FormatEmail = void 0;
-function FormatEmail(email) {
-    const emaillower = email.toLowerCase();
-    return emaillower.trim();
-}
-exports.FormatEmail = FormatEmail;
