26fc1856d614d41c619da318e9a39af309d141cf
Gabriel Fernandes <64819066+ferndsgabriel@users.noreply.github.com>
2024-09-25 21:39:51 -0300

    Fix: data equals

--- src/Services/concierge/FilterTodayReservationsServices.ts
+++ src/Services/concierge/FilterTodayReservationsServices.ts
@@ -1,68 +1,59 @@
-import { lte } from "lodash";
-import prismaClient from "../../prisma"
+import prismaClient from "../../prisma";
 
-
-
-class FilterTodayReservationsServices{
-    async execute(){
-        const addZero = (value:number) =>{
-            if(value < 10){
-                return `0${value}`
-            }else{
-                return `${value}`
-            }
-        }
+class FilterTodayReservationsServices {
+    async execute() {
+        const addZero = (value) => (value < 10 ? `0${value}` : `${value}`);
 
         const date = new Date();
-
         const year = date.getFullYear();
-        const month = date.getMonth() + 1;
+        const month = date.getMonth() + 1; 
         const day = date.getDate();
 
-        const data = `${addZero(year)}${addZero(month)}${addZero(day)}`
+        
+        const data = `${year}${addZero(month)}${addZero(day)}`;
         const dateInt = parseInt(data);
 
         const getReservations = await prismaClient.reservation.findMany({
-            where:{
-                date:{
-                    equals:dateInt
+            where: {
+                date: {
+                    equals: dateInt,
                 },
-                reservationStatus:true
-            },select:{
-                id:true,
-                apartment:{
-                    select:{
-                        numberApt:true,
-                        tower:{
-                            select:{
-                                numberTower:true
-                            }
-                        }
-                    }
-                },cleaningService:true,
-                date:true,
-                start:true,
-                finish:true,
-                name:true,
-                phone_number:true,
-                GuestList:{
-                    select:{
-                        attended:true,
-                        id:true,
-                        name:true,
-                        rg:true
-                    }
-                }
+                reservationStatus: true,
             },
-            orderBy:{
-                date:"desc"
-            }
+            select: {
+                id: true,
+                apartment: {
+                    select: {
+                        numberApt: true,
+                        tower: {
+                            select: {
+                                numberTower: true,
+                            },
+                        },
+                    },
+                },
+                cleaningService: true,
+                date: true,
+                start: true,
+                finish: true,
+                name: true,
+                phone_number: true,
+                GuestList: {
+                    select: {
+                        attended: true,
+                        id: true,
+                        name: true,
+                        rg: true,
+                    },
+                },
+            },
+            orderBy: {
+                date: "desc",
+            },
         });
 
-
         return getReservations;
-
     }
 }
 
-export {FilterTodayReservationsServices}
+export { FilterTodayReservationsServices };
