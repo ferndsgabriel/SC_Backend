52b54daca03c3275e7c6bf01c852478571547c9a
Gabriel Fernandes <64819066+ferndsgabriel@users.noreply.github.com>
2024-09-25 21:46:05 -0300

    Update FilterTodayReservationsServices.ts

--- src/Services/concierge/FilterTodayReservationsServices.ts
+++ src/Services/concierge/FilterTodayReservationsServices.ts
@@ -6,11 +6,16 @@ class FilterTodayReservationsServices {
             return value < 10 ? `0${value}` : `${value}`;
         };
 
-        const date = new Date();
-        const year = date.getFullYear();
-        const month = date.getMonth() + 1; 
-        const day = date.getDate();
+        // Obter a data atual considerando o fuso horário de Brasília (UTC-3 ou UTC-2)
+        const today = new Date();
+        const utcOffset = today.getTimezoneOffset() * 60000; // em milissegundos
+        const brasiliaTime = new Date(today.getTime() + utcOffset + (3 * 3600000)); // Ajustando para UTC-3
 
+        const year = brasiliaTime.getFullYear();
+        const month = brasiliaTime.getMonth() + 1; // Lembre-se que o mês é zero-indexado
+        const day = brasiliaTime.getDate();
+
+        // Formato YYYYMMDD
         const data = `${year}${addZero(month)}${addZero(day)}`;
         const dateInt = parseInt(data);
 
